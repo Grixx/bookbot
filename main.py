295cd45441add2bebe9f2483425c6ac099d97805
Grixx <jbullardgit@fastmail.com>
2025-02-02 23:01:00 -0500

    added counters for alphabet chars

--- main.py
+++ main.py
@@ -4,11 +4,18 @@ def main():
     num_words = get_num_words(text)
     
     char_counts  = character_counts(text)
-    print(char_counts)
+    alpha_counts = []
+    for c,v in char_counts.items():
+       if c.isalpha():
+           alpha_counts.append({"char":c, "count":v})
+    alpha_counts.sort(reverse=True, key=sort_on)    
+    
+    
+    print(f"--- Begin report of {book_path} ---")
+    print (f"This book contains {num_words} words\n")
 
-    print(f"--- Begin report of {book_path}")
-    print (f"This book contains {num_words} words")
-
+    for c in range(len(alpha_counts)):
+     print(f"The '{alpha_counts[c]["char"]}' character was found {alpha_counts[c]["count"]} times.")
     
 
 
@@ -20,11 +27,14 @@ def get_num_words(text):
 def get_book_text(path):    
     with open(path) as f:
         return f.read()
+    
+def sort_on(dict):
+    return dict["count"]
 
 def character_counts(text):
     characters = {}
+    lower_string = text.lower()
     for w in lower_string:
-        lower_string = text.lower()
         if w in characters:
             characters[w] = characters[w] + 1
         else:
